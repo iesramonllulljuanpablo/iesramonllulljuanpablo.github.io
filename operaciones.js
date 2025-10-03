05901dbe7f1c536c0d9de9d1e27128c5137e20f3
iesramonllulljuanpablo <al.juanpablomontoya@iesramonllull.net>
2025-10-03 09:47:04 +0200

    1.6

---
+++ operaciones.js
@@ -0,0 +1,12 @@
+function restar() {
+    let valor1 = parseInt(document.getElementById('numero1').value);
+    let valor2 = parseInt(document.getElementById('numero2').value);
+    let resultado = valor1 - valor2;
+    alert("El resultado es " + resultado);
+}
+function sumar() {
+    let valor1 = parseInt(document.getElementById('numero1').value);
+    let valor2 = parseInt(document.getElementById('numero2').value);
+    let resultado = valor1 + valor2;
+    alert("El resultado es " + resultado);
+}
